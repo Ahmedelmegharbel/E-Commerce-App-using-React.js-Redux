5115e60b1334f35ece472a9275c266ef51ee6e76
Ahmedelmegharbel <ahmedelmegharbel@gmail.com>
2025-12-29 17:17:04 +0200

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,20 @@
+// App.js
+import React from 'react';
+import ProductList from './Components/ProductList';
+import ShoppingCart from './Components/ShoppingCart';
+import './App.css'
+import SuperCoins from './Components/SuperCoin';
+const App = () => {
+  return (
+ 
+      <div>
+        <h1 className='app-heading'>E-Commerce Application</h1>
+        <ProductList />
+        <ShoppingCart />
+        <SuperCoins />
+      </div>
+
+  );
+};
+
+export default App;
